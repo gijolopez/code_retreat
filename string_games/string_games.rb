6a4738114391940398686badf17f1934b0437455
gijolopez <glopez22@me.com>
2018-02-08 15:56:33 -0700

    added downcase feature to method

--- string_games/string_games.rb
+++ string_games/string_games.rb
@@ -3,16 +3,19 @@ class StringGames
 
   def palindrome?(word)
     backwards = reverse(word)
-    word.chars == backwards
+    word.downcase.chars == backwards
   end
 
   def reverse(word)
     backwards = []
     index = 0
     while index < word.length
-      backwards << word[(index + 1) * -1]
+      backwards << word.downcase[(index + 1) * -1]
       index += 1
     end
     backwards
   end
 end
+
+sg = StringGames.new
+p sg.palindrome? ("Wow")
