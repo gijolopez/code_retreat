a3f0a08161526cb777288ca54a899aabfd93cc3d
gijolopez <glopez22@me.com>
2018-02-08 14:26:53 -0700

    created palindrome method to validate word

---
+++ string_games/string_games.rb
@@ -0,0 +1,28 @@
+class StringGames
+
+  def palindrome?(word)
+    word == word.reverse
+
+  end
+end
+=begin
+anna
+Civic
+Kayak
+Level
+Madam
+Mom
+Noon
+Racecar
+Radar
+Redder
+Refer
+Repaper
+Rotator
+Rotor
+Sagas
+Solos
+Stats
+Tenet
+Wow
+=end
---
+++ string_games/string_games_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'string_games'
+
+class StringGamesTest < Minitest::Test
+
+  def test_it_exist
+    sg = StringGames.new
+
+    assert_instance_of StringGames, sg
+  end
+
+  def test_if_palindorome
+    sg = StringGames.new
+    word = "wow"
+
+    assert sg.palindrome?(word)
+  end
+
+  def test_if_not_palindorome
+    sg = StringGames.new
+    word = "George"
+
+    refute sg.palindrome?(word)
+  end
+
+end
